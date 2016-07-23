53a673b4222277b9307973fcae8510b780b618a2
Alpheus Masanga <alklyn@gmail.com>
2016-07-23 19:27:02 +0200

    Remove references to flask-bootstrap.

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,8 @@
 from flask import Flask, session
-from flask_bootstrap import Bootstrap
 from flask_login import LoginManager, UserMixin, login_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Y0uWillN3v3RGue55'
-Bootstrap(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
