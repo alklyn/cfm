8427fe91e27c1a816994df838a3fb8df2e4d895f
Alpheus Masanga <alklyn@gmail.com>
2016-07-31 01:45:06 +0200

    Hide submit unless all fiels filled.

--- app/forms.py
+++ app/forms.py
@@ -26,85 +26,95 @@ class CreateTicketForm(Form):
     caller_firstname = TextField('First name of caller',
                                  validators=[InputRequired()],
                                  render_kw={"placeholder": "Name of caller",
-                                            "class": "ticket_element"})
+                                            "class": "ticket_element",
+                                            "onKeyUp": "hideUnhideSubmit(this.id, this.form)"})
 
     caller_lastname = TextField('Last name of caller',
                                 validators=[InputRequired()],
                                 render_kw={"placeholder": "Surname of caller",
-                                           "class": "ticket_element"})
+                                           "class": "ticket_element",
+                                           "onKeyUp": "hideUnhideSubmit(this.id, this.form)"})
 
     phone_number = TextField('Phone Number',
                              validators=[InputRequired()],
                              render_kw={"placeholder": "Phone No of caller",
-                                        "class": "ticket_element"})
+                                        "class": "ticket_element",
+                                        "onKeyUp": "hideUnhideSubmit(this.id, this.form)"})
 
     genders = prep_select("gender")  #list of id, gender tuples
     gender = SelectField('Gender',
                          validators=[InputRequired()],
                          choices=genders,
-                         render_kw={"class": "ticket_element"})
+                         render_kw={"class": "ticket_element",
+                         "onChange": "hideUnhideSubmit(this.id, this.form)"})
 
     provinces = prep_select("province")  #list of id, province tuples
     province = SelectField('Province',
                            validators=[InputRequired()],
                            choices=provinces,
-                           render_kw={"onchange": "this.form.submit()"})
+                           render_kw={"onChange": "hideUnhideSubmit(this.id, this.form)"})
 
     districts = []  #list of id, district tuples
     district = SelectField('District',
                            validators=[InputRequired()],
                            choices=districts,
-                           render_kw={"onchange": "this.form.submit()"})
+                           render_kw={"onchange": "hideUnhideSubmit(this.id, this.form)"})
 
     wards = []
     ward = SelectField('Ward',
                        validators=[InputRequired()],
                        choices=wards,
-                       render_kw={"onchange": "this.form.submit()",
+                       render_kw={"onchange": "hideUnhideSubmit(this.id, this.form)",
                                   "class": "ticket_element"})
 
     villages = []
     village = SelectField('Village',
                           validators=[InputRequired()],
                           choices=villages,
-                          render_kw={"class": "ticket_element"})
+                          render_kw={"class": "ticket_element",
+                                     "onChange": "hideUnhideSubmit(this.id, this.form)"})
 
 
     partners = prep_select("partner")  #list of id, partner tuples
     partner = SelectField('Cooperating Partner',
                           validators=[InputRequired()],
                           choices=partners,
-                          render_kw={"class": "ticket_element"})
+                          render_kw={"class": "ticket_element",
+                          "onChange": "hideUnhideSubmit(this.id, this.form)"})
 
     programmes = prep_select("programme")  #list of id, programme tuples
     programme = SelectField('Programme',
                             validators=[InputRequired()],
                             choices=programmes,
-                            render_kw={"class": "ticket_element"})
+                            render_kw={"class": "ticket_element",
+                            "onChange": "hideUnhideSubmit(this.id, this.form)"})
 
     topics = prep_select("topic")  #list of id, topic tuples
     topic = SelectField('Help Topic',
                         validators=[InputRequired()],
                         choices=topics,
-                        render_kw={"class": "ticket_element"})
+                        render_kw={"class": "ticket_element",
+                                   "onChange": "hideUnhideSubmit(this.id, this.form)"})
 
     prioritys = prep_select("priority")  #list of id, priority tuples
     priority = SelectField('Priority',
                            validators=[InputRequired()],
                            choices=prioritys,
-                           render_kw={"class": "ticket_element"})
+                           render_kw={"class": "ticket_element",
+                           "onChange": "hideUnhideSubmit(this.id, this.form)"})
 
     #staffmember assigned ticket
     agents = prep_select("user")  #list of id, fullname tuples
     agent = SelectField('Assign to',
                         validators=[InputRequired()],
                         choices=agents,
-                        render_kw={"class": "ticket_element"})
+                        render_kw={"class": "ticket_element",
+                        "onChange": "hideUnhideSubmit(this.id, this.form)"})
 
     details = TextAreaField('Issue Details',
                             [validators.InputRequired()],
                             render_kw={"placeholder": "query/complaint",
                                        "class": "ticket_element",
-                                       "onKeyUp": "updateOpenDiv(this.id)"})
+                                       "onKeyUp": "hideUnhideSubmit(this.id)"})
 
     open_ticket = SubmitField('Open ticket')
--- app/static/js/ticket.js
+++ app/static/js/ticket.js
@@ -1,13 +1,61 @@
-function updateOpenDiv(thisid){
+function hideUnhideSubmit(thisId, thisForm){
     /*
-    Update the div containing the create_ticket button based on a change
-    on the calling element.
+    Show the submit button only after all the fields have been filled
     */
     var oldClass = "";
     var newClass = "";
-    eid = "open_ticket_div";
-    openBtn = document.getElementById(thisid)
-    if(openBtn.value.length > 4){
+    var isFilled = true;
+    eid = "open_ticket_div"
+
+    var elements = document.querySelectorAll(".ticket_element")
+    var minChars = 2;  //Minimum number of characters
+    for (var i = 0; i < elements.length; i++){
+        //Check that all the elements have been filled in
+        currentID = elements[i].id;
+        element = document.getElementById(currentID);
+        switch (currentID) {
+            case "caller_firstname":
+            case "caller_lastname":
+                minChars = 2;
+                break;
+
+            case "phone_number":
+                minChars = 6;
+                break;
+
+            case "province":
+            case "district":
+            case "ward":
+                notVal = "0";
+                thisForm.submit();
+                break;
+
+            case "village":
+            case "gender":
+            case "partner":
+            case "programme":
+            case "topic":
+            case "priority":
+            case "agent":
+                notVal = "0";
+                break;
+
+            default:
+                //
+
+        }
+        var type = element.type.toLowerCase();
+        if ((type == "text") || (type == "textarea")){ //Text
+            isFilled = (element.value.length >= minChars);
+        }
+        else {
+            isFilled = (element.value != notVal);
+        }
+        if(!isFilled){
+            break;
+        }
+    }
+    if (isFilled){
         // alert('You typed ' + openBtn.value);
         oldClass = "hide";
         newClass = "show";
@@ -18,6 +66,7 @@ function updateOpenDiv(thisid){
     }
     changeCSSClass(eid, oldClass, newClass);
 }
+
 
 function changeCSSClass(eid, oldClass, newClass){
     /*
