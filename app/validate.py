0bbdc4046d26e16880f5c0964821ad09549a998a
Alpheus Masanga <alklyn@gmail.com>
2016-08-01 22:37:52 +0200

    Move "update_selectors()" to validate.py.

---
+++ app/validate.py
@@ -0,0 +1,52 @@
+"""
+A collection of procedures for validating user input.
+"""
+from flask import request
+from werkzeug.exceptions import HTTPException
+from app.dbi import prep_select
+from app.forms import TicketForm
+
+
+def update_selectors():
+    """
+    Update create_ticket form to display the location selector that is
+    dependent on the one just selected.
+    For example the selector for district will not be shown until the selector
+    for province has been set
+    """
+    form = TicketForm()
+    province_set = False
+    district_set = False
+    ward_set = False
+    try:  #Check if the province is selected
+        if request.form["province"] != "0":
+            province_set = True
+            province_id = int(request.form["province"])
+            #list of id, district tuples
+            districts = prep_select(table="district",
+                                    constraint=province_id)
+            form.district.choices = districts
+    except (AttributeError, NameError, HTTPException) as error:
+        message = str(error)
+
+    try:  #Check if the district is selected
+        if request.form["district"] != "0":
+            district_set = True
+            district_id = int(request.form["district"])
+            #list of id, ward tuples
+            wards = prep_select(table="ward", constraint=district_id)
+            form.ward.choices = wards
+    except (AttributeError, NameError, HTTPException) as error:
+        message = str(error)
+
+    try:  #Check if the ward is selected
+        if request.form["ward"] != "0":
+            ward_set = True
+            ward_id = int(request.form["ward"])
+            #list of id, village tuples
+            villages = prep_select(table="village", constraint=ward_id)
+            form.village.choices = villages
+    except (AttributeError, NameError, HTTPException) as error:
+        message = str(error)
+
+    return form, province_set, district_set, ward_set
--- app/views.py
+++ app/views.py
@@ -1,14 +1,14 @@
 """ All the views in the the app are here """
 from flask import render_template, flash, redirect, request, url_for, session
-from app import app
-from app import dbi
-from app.forms import LoginForm, TicketForm
 from wtforms import TextField, SubmitField, PasswordField, SelectField, \
                     TextAreaField, validators
 from wtforms.validators import Required
 from werkzeug.exceptions import HTTPException
+from app import app
+from app import dbi
 from app.dbi import prep_select
-import sys
+from app.forms import LoginForm, TicketForm
+from app.validate import update_selectors
 
 
 @app.route('/', methods=["GET", "POST"])
@@ -135,48 +135,3 @@ def create_ticket():
                            district_set=district_set,
                            ward_set=ward_set,
                            country_code=country_code)
-
-
-def update_selectors():
-    """
-    Update create_ticket form to display the location selector that is
-    dependent on the one just selected.
-    For example the selector for district will not be shown until the selector
-    for province has been set
-    """
-    form = TicketForm()
-    province_set = False
-    district_set = False
-    ward_set = False
-    try:  #Check if the province is selected
-        if request.form["province"] != "0":
-            province_set = True
-            province_id = int(request.form["province"])
-            #list of id, district tuples
-            districts = prep_select(table="district",
-                                    constraint=province_id)
-            form.district.choices = districts
-    except (AttributeError, NameError, HTTPException) as error:
-        message = str(error)
-
-    try:  #Check if the district is selected
-        if request.form["district"] != "0":
-            district_set = True
-            district_id = int(request.form["district"])
-            #list of id, ward tuples
-            wards = prep_select(table="ward", constraint=district_id)
-            form.ward.choices = wards
-    except (AttributeError, NameError, HTTPException) as error:
-        message = str(error)
-
-    try:  #Check if the ward is selected
-        if request.form["ward"] != "0":
-            ward_set = True
-            ward_id = int(request.form["ward"])
-            #list of id, village tuples
-            villages = prep_select(table="village", constraint=ward_id)
-            form.village.choices = villages
-    except (AttributeError, NameError, HTTPException) as error:
-        message = str(error)
-
-    return form, province_set, district_set, ward_set
